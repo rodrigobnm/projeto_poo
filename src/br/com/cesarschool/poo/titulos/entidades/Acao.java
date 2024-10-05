8699557fe9a661c013bd1369b33e55f580270c63
Rodrigo <you@example.com>
2024-10-05 17:37:58 -0300

    bugs corrigido

--- src/br/com/cesarschool/poo/titulos/entidades/Acao.java
+++ src/br/com/cesarschool/poo/titulos/entidades/Acao.java
@@ -1,15 +1,22 @@
 package br.com.cesarschool.poo.titulos.entidades;
-/*
- * Esta classe deve herdar de Ativo.
- * E deve ter os seguintes atributos:
- * valorUnitario, do tipo double.
- * 
- * Deve ter um construtor público que inicializa os atributos, 
- * e métodos set/get públicos para os atributos.
- * 
- * Deve ter um método público double calcularPrecoTransacao(double montante): o preço 
- * da transação é o produto do montante pelo valorUnitario.
- */
-public class Acao {
 
+public class Acao extends Ativo {
+    private double valorUnitario;
+
+    public Acao(int identificador, String nome, java.time.LocalDate dataValidade, double valorUnitario) {
+        super(identificador, nome, dataValidade);
+        this.valorUnitario = valorUnitario;
+    }
+
+    public double getValorUnitario() {
+        return valorUnitario;
+    }
+
+    public void setValorUnitario(double valorUnitario) {
+        this.valorUnitario = valorUnitario;
+    }
+
+    public double calcularPrecoTransacao(double montante) {
+        return montante * valorUnitario;
+    }
 }
