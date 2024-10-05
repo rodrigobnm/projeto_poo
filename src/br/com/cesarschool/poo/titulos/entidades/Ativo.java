c7963f466b646c9d28cb376d8ee7ef8f67141767
Rodrigo <you@example.com>
2024-10-05 17:20:04 -0300

    tudo ok

--- src/br/com/cesarschool/poo/titulos/entidades/Ativo.java
+++ src/br/com/cesarschool/poo/titulos/entidades/Ativo.java
@@ -1,14 +1,35 @@
 package br.com.cesarschool.poo.titulos.entidades;
-/*
- * Esta classe deve ter os seguintes atributos:
- * identificador, do tipo int
- * nome, do tipo String
- * data de validade, do tipo LocalDate
- * 
- * Deve ter um construtor público que inicializa os atributos, 
- * e métodos set/get públicos para os atributos. O atributo identificador
- * é read-only fora da classe.
- */
-public class Ativo {
 
+import java.time.LocalDate;
+
+public class Ativo {
+    private int identificador;
+    private String nome;
+    private LocalDate dataValidade;
+    
+    public Ativo(int identificador, String nome, LocalDate dataValidade) {
+        this.identificador = identificador;
+        this.nome = nome;
+        this.dataValidade = dataValidade;
+    }
+    
+    public int getIdentificador() {
+        return identificador;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataValidade() {
+        return dataValidade;
+    }
+
+    public void setDataValidade(LocalDate dataValidade) {
+        this.dataValidade = dataValidade;
+    }
 }
--- src/br/com/cesarschool/poo/titulos/entidades/EntidadeOperadora.java
+++ src/br/com/cesarschool/poo/titulos/entidades/EntidadeOperadora.java
@@ -1,25 +1,61 @@
 package br.com.cesarschool.poo.titulos.entidades;
-/*
- * Esta classe deve ter os seguintes atributos:
- * identificador, do tipo long
- * nome, do tipo String
- * autorizadoAcao, do tipo double
- * saldoAcao, do tipo double
- * saldoTituloDivida, do tipo double
- * 
- * Deve ter um construtor público que inicializa os atributos identificador, nome
- * e autorizadoAcao. Deve ter métodos set/get públicos para os atributos identificador, nome
- * e autorizadoAcao. O atributo identificador é read-only fora da classe.
- * 
- * Os atributos saldoAcao e saldoTituloDivida devem ter apenas métodos get públicos.
- * 
- * Outros métodos públicos:
- * 
- *  void creditarSaldoAcao(double valor): deve adicionar valor ao saldoAcao
- *  void debitarSaldoAcao(double valor): deve diminuir valor de saldoAcao
- *  void creditarSaldoTituloDivida(double valor): deve adicionar valor ao saldoTituloDivida
- *  void debitarSaldoTituloDivida(double valor): deve diminuir valor de saldoTituloDivida  
- */
-public class EntidadeOperadora {
 
+public class EntidadeOperadora {
+    private long identificador;
+    private String nome;
+    private double autorizadoAcao;
+    private double saldoAcao;
+    private double saldoTituloDivida;
+
+    public EntidadeOperadora(long identificador, String nome, double autorizadoAcao) {
+        this.identificador = identificador;
+        this.nome = nome;
+        this.autorizadoAcao = autorizadoAcao;
+        this.saldoAcao = 0;
+        this.saldoTituloDivida = 0;
+    }
+
+    public long getIdentificador() {
+        return identificador;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getAutorizadoAcao() {
+        return autorizadoAcao;
+    }
+
+    public void setAutorizadoAcao(double autorizadoAcao) {
+        this.autorizadoAcao = autorizadoAcao;
+    }
+
+    public double getSaldoAcao() {
+        return saldoAcao;
+    }
+
+    public double getSaldoTituloDivida() {
+        return saldoTituloDivida;
+    }
+
+    public void creditarSaldoAcao(double valor) {
+        this.saldoAcao += valor;
+    }
+
+    public void debitarSaldoAcao(double valor) {
+        this.saldoAcao -= valor;
+    }
+
+    public void creditarSaldoTituloDivida(double valor) {
+        this.saldoTituloDivida += valor;
+    }
+
+    public void debitarSaldoTituloDivida(double valor) {
+        this.saldoTituloDivida -= valor;
+    }
 }
